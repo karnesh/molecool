14627bf9e93f60974020b5093a9a94e39563fa0e
Karnesh Jain <jain.karnesh@gmail.com>
2020-01-31 23:48:30 -0600

    run yapf on molecool

--- molecool/measure.py
+++ molecool/measure.py
@@ -4,38 +4,33 @@
 
 import numpy as np
 
+
 def calculate_distance(rA, rB):
-    """Calculate the distance between two points.
+    """
+    Calculate the distance between two points.
         
-        Parameters
-        ----------
-        rA, rB : np.ndarray
-        The coordinates of each point.
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+    The coordinates of each point.
         
-        Returns
-        -------
-        distance : float
-        The distance between the two points.
-        
-        Examples
-        --------
-        >>> r1 = np.array([0, 0, 0])
-        >>> r2 = np.array([0, 0.1, 0])
-        >>> calculate_distance(r1, r2)
-        0.1
-        """
-    
+    Returns
+    -------
+    distance : float
+    The distance between the two points.
+    """
+
     dist_vec = (rA - rB)
     distance = np.linalg.norm(dist_vec)
-    
     return distance
+
 
 def calculate_angle(rA, rB, rC, degrees=False):
     AB = rB - rA
     BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
-    
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
+
     if degrees:
         return np.degrees(theta)
     else:
-    return theta
+        return theta
