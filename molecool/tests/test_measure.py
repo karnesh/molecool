7afcf8d29cf786b3a1fcf88036da74961210b9f0
Karnesh Jain <jain.karnesh@gmail.com>
2020-02-02 09:21:39 -0600

    test updated

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -49,3 +49,11 @@ def test_center_of_mass():
     expected_center = np.array([1,1,1])
     
     assert np.array_equal(center_of_mass, expected_center)
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle", [(np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45), (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60  ), (np.array([np.sqrt(3)/2, (1/2), 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 30),
+                                                    ])
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+    
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+    
+    assert expected_angle == pytest.approx(calculated_angle), F'{calculated_angle} {expected_angle}'
