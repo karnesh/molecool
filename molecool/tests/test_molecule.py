a72705fbd7f3fee39e557f39aaff02169f758c8d
Karnesh Jain <jain.karnesh@gmail.com>
2020-02-02 09:03:11 -0600

    added unit tests

--- molecool/tests/test_molecule.py
+++ molecool/tests/test_molecule.py
@@ -8,6 +8,12 @@ import pytest
 import sys
 import numpy as np
 
+@pytest.fixture
+def methane_molecule():
+    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+    return symbols, coordinates
+
 def test_build_bond_list():
     
     coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
@@ -28,3 +34,18 @@ def test_molecular_mass():
         molecool.atom_data.atomic_weights['H'] + molecool.atom_data.atomic_weights['H']+ molecool.atom_data.atomic_weights['H']
     
     assert actual_mass == calculated_mass
+
+def test_build_bond_list_failure():
+    coordinates = np.array([])
+    
+    with pytest.raises(ValueError):
+        molecool.build_bond_list(coordinates)
+
+def test_center_of_mass(methane_molecule):
+    symbols, coordinates = methane_molecule
+    
+    center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
+    
+    expected_center = np.array([1,1,1])
+    
+    assert np.array_equal(center_of_mass, expected_center)
